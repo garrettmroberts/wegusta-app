ffba651f886d4b79dc5a91e80b968330bfee9930
Garrett Roberts <garrettroberts@Garretts-MacBook-Air.local>
2021-05-06 21:02:14 -0600

    file structure init + eslint

---
+++ app/config/colors.js
@@ -0,0 +1,5 @@
+export default {
+    primary: '#0A0A0A',
+    seconary: '#FF004D',
+    black: '#000'
+}
---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+export default function HomeScreen() {
+
+  return (
+      <View style={styles.container} >
+        <Text>Hello Native!</Text>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
