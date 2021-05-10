3dc41812aa2584cb8c10edab6c0c3ed205c1cadc
Garrett Roberts <garrettroberts@Garretts-MacBook-Air.local>
2021-05-10 17:14:53 -0600

    changed ImageBlock to function based component

--- app/components/ImageBlock/ImageBlock.jsx
+++ app/components/ImageBlock/ImageBlock.jsx
@@ -3,49 +3,47 @@ import { View, Image } from 'react-native';
 
 import styles from './styles';
 
-class ImageBlock extends Component {
-  render() {
-    return (
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.imageStyle}
-          source={{
-            uri: 'https://picsum.photos/500/600'
-          }}
-        />
-        <Image
-          style={styles.imageStyle}
-          source={{
-            uri: 'https://picsum.photos/500/600'
-          }}
-        />
-        <Image
-          style={styles.imageStyle}
-          source={{
-            uri: 'https://picsum.photos/500/600'
-          }}
-        />
-        <Image
-          style={styles.imageStyle}
-          source={{
-            uri: 'https://picsum.photos/500/600'
-          }}
-        />
-        <Image
-          style={styles.imageStyle}
-          source={{
-            uri: 'https://picsum.photos/500/600'
-          }}
-        />
-        <Image
-          style={styles.imageStyle}
-          source={{
-            uri: 'https://picsum.photos/500/600'
-          }}
-        />
-      </View>
-    );
-  }
-}
+const ImageBlock = () => {
+  return (
+    <View style={styles.imageContainer}>
+      <Image
+        style={styles.imageStyle}
+        source={{
+          uri: 'https://picsum.photos/500/600'
+        }}
+      />
+      <Image
+        style={styles.imageStyle}
+        source={{
+          uri: 'https://picsum.photos/500/600'
+        }}
+      />
+      <Image
+        style={styles.imageStyle}
+        source={{
+          uri: 'https://picsum.photos/500/600'
+        }}
+      />
+      <Image
+        style={styles.imageStyle}
+        source={{
+          uri: 'https://picsum.photos/500/600'
+        }}
+      />
+      <Image
+        style={styles.imageStyle}
+        source={{
+          uri: 'https://picsum.photos/500/600'
+        }}
+      />
+      <Image
+        style={styles.imageStyle}
+        source={{
+          uri: 'https://picsum.photos/500/600'
+        }}
+      />
+    </View>
+  );
+};
 
 export { ImageBlock };
