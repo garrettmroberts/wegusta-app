7df29e4754eefa3e0178da6342c52bca1e6137c2
Garrett Roberts <garrettroberts@Garretts-MacBook-Air.local>
2021-05-09 18:37:52 -0600

    consolidated card, buttonCard, and iconCard into same component

--- app/tests/components/Button.spec.js
+++ app/tests/components/Button.spec.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { shallow,mount } from 'enzyme';
 import { Text, Pressable } from 'react-native';
 import Button from '../../components/Button';
 
 describe('Button Component', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(<Button text="Balyhoo" />).toJSON();
-    expect(tree).toMatchSnapshot();
+  describe('DEFAULT', () => {
+    it('renders default display correctly', () => {
+      const tree = renderer.create(<Button text="Balyhoo" />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  
+    it('Displays text prop in button', () => {
+      const button = shallow(<Button text="Ballyhoo" />);
+      expect(button.contains(<Text>Ballyhoo</Text>));
+    });
+  
+    it('passes extra props to Pressable component', () => {
+      const button = shallow(<Button text="Ballyhoo" prop_1="true" />);
+      const pressable = button.find(Pressable);
+      expect(pressable.props().prop_1).toEqual('true');
+    });
   });
 
-  it('Displays text prop in button', () => {
-    const button = shallow(<Button text="Ballyhoo" />);
-    expect(button.contains(<Text>Ballyhoo</Text>));
-  });
-
-  it('passes extra props to Pressable component', () => {
-    const button = shallow(<Button text="Ballyhoo" prop_1="true" />);
-    const pressable = button.find(Pressable);
-    expect(pressable.props().prop_1).toEqual('true');
-  });
+  // describe('CARD', () => {
+  //   it('renders display correctly when type === card', () => {
+  //     const button = mount(<Button type="card" />);
+  //     console.log(button.props().style());
+  //   });
+  // });
+ 
 });
