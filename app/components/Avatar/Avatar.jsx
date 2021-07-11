1142c46eede51692eea96b0e260368cb6263c16c
Garrett Roberts <garrettroberts@Garretts-MacBook-Air.local>
2021-07-11 17:13:45 -0600

    avatar component

---
+++ app/components/Avatar/Avatar.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Image, Text, Pressable } from 'react-native';
+import PropTypes from 'prop-types';
+
+import styles from './styles';
+import buildIcon from '../../utils/buildIcon';
+import { colors } from '../../config';
+
+const Avatar = ({ size, avatarStyle, accessory, letter, image, onPress }) => {
+  const formatIcon = (size) => {
+    switch (size) {
+    case 'large':
+      return buildIcon({ name: 'md-person-add', size: 35, color: colors.white });
+    case 'medium':
+      return buildIcon({ name: 'md-person-add', size: 28, color: colors.white });
+    case 'small':
+      return buildIcon({ name: 'md-person-add', size: 21, color: colors.white });
+    case 'xsmall':
+      return buildIcon({ name: 'md-person-add', size: 12, color: colors.white });
+    }
+  };
+
+  const accesorize = (accessory, size) => {
+    if (accessory === 'checkmark' && size !== 'xsmall') {
+      return (
+        <View style={[styles.accessory, styles[`accessory${size}`]]}>
+          { buildIcon({ name: 'checkmark', size: 20, color: colors.white }) }
+        </View>
+      );
+    }
+  };
+
+  const setContent = (avatarStyle, size, letter, image) => {
+    if (avatarStyle === 'image') {
+      return <Image source={image} resizeMode="cover" style={[styles[size], styles.image]} />;
+    } else if (avatarStyle === 'letter') {
+      return <Text style={[styles.letter, styles[`letter${size}`]]}>{ letter }</Text>;
+    } else {
+      return formatIcon(size);
+    }
+  };
+
+  return (
+    <Pressable onPress={onPress} style={styles[size]}>
+      <View style={[styles.wrapper, styles[size]]}>
+        { setContent(avatarStyle, size, letter, image) }
+      </View>
+      { accesorize(accessory, size) }
+    </Pressable>
+  );
+};
+
+Avatar.propTypes = {
+  size: PropTypes.string.isRequired, // large, medium, small, xsmall
+  avatarStyle: PropTypes.string, // image, letter
+  letter: PropTypes.string, // Only required if avatarStyle === 'letter'
+  image: PropTypes.object, // Only required if avatarStyle === 'image'.  Check react-native Image docs for example
+  accessory: PropTypes.string, // checkmark
+  onPress: PropTypes.func
+};
+
+export { Avatar };
--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,4 @@
+export * from './Avatar/Avatar';
 export * from './Button/Button';
 export * from './IconButton/IconButton';
 export * from './DecisionButton/DecisionButton';
