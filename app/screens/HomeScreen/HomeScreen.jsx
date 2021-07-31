47486caef71248a618ef7608f6901edb261682e6
Garrett Roberts <garrettroberts@Garretts-MacBook-Air.local>
2021-07-31 10:50:26 -0600

    make plans button navigates to componentsDemo

--- app/screens/HomeScreen/HomeScreen.jsx
+++ app/screens/HomeScreen/HomeScreen.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import { View, ImageBackground, Text } from 'react-native';
+import { PropTypes } from 'prop-types';
 
 import Button from '../../components/Button/Button';
 import styles from './styles';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+  const navigateButton = () => {
+    navigation.navigate('ComponentsDemo');
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground source={require('../../assets/homescreenBackground.png')} resizeMode="cover" style={styles.image}>
         <View style={styles.wrapper}>
           <Text style={styles.header}>No more nose goes...</Text>
           <Text style={styles.subtext}>Find the perfect restaurant for everyone.</Text>
-          <Button type='primary' size='large' text='Make Plans' iconPlacement="right" icon="arrow-forward" />
+          <Button type='primary' size='large' text='Make Plans' iconPlacement="right" icon="arrow-forward" handlePress={navigateButton} />
         </View>
       </ImageBackground>
     </View>
   );
+};
+
+HomeScreen.propTypes = {
+  navigation: PropTypes.object
 };
 
 export default HomeScreen;
