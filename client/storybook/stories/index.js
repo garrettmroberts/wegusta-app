f65fb83a07aadeee494e9341eadf1bad0777ce08
Garrett Roberts <garrettmroberts@gmail.com>
2022-05-22 09:16:54 -0600

    Switch component storybook

--- client/storybook/stories/index.js
+++ client/storybook/stories/index.js
@@ -1,3 +1,5 @@
 import './Welcome/Welcome.stories'
-import '../../app/components/Toast/Toast.stories'
 import '../../app/components/SocialButton/SocialButton.stories'
+import '../../app/components/Switch/Switch.stories'
+import '../../app/components/Toast/Toast.stories'
+
