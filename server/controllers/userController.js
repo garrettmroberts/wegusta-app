7a2b212bc0fe7a3531e7e86d158b8e8a471e0aed
Garrett Roberts <garrettmroberts@gmail.com>
2022-06-13 19:10:50 -0600

    signup api error handling

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,9 +3,16 @@ const User = require('../models/userModel');
 
 module.exports = {
   createUser: async (req, res) => {
-    db.collection("users").add(req.body)
-    .then(dbResponse => res.status(201).json({ message: "Record saved successfully", data: dbResponse }))
-    .catch(err => res.status(422).json(err));
+    const docRef = await db.collection('users').doc(req.body.phoneNumber)
+    docRef.get().then(doc => {
+      if (doc.exists) {
+        res.status(400).json({ message: 'This phone number is already registered with another account.'})
+      } else {
+        db.collection("users").doc(req.body.phoneNumber).set(req.body)
+        .then(dbResponse => res.status(201).json({ message: "Record saved successfully", data: dbResponse }))
+        .catch(err => res.status(422).json(err));
+      }
+    })
   },
 
   findAll: (req, res) => {
@@ -18,7 +25,8 @@ module.exports = {
           doc.id,
           doc.data().authId,
           doc.data().color,
-          doc.data().name
+          doc.data().name,
+          doc.data().phoneNumber
         );
         console.log(user)
         response.push(user);
