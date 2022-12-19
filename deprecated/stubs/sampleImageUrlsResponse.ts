720b774246549ca3a613bee2d79933460c564a92
Garrett <garrettmroberts@gmail.com>
2022-12-19 12:38:54 -0700

    store images locally

---
+++ deprecated/stubs/sampleImageUrlsResponse.ts
@@ -0,0 +1,34 @@
+export default [
+  {
+    category: 'deli',
+    uri: 'deli1'
+  },
+  {
+    category: 'french',
+    uri: 'french1'
+  },
+  {
+    category: 'sushi',
+    uri: 'sushi1'
+  },
+  {
+    category: 'pizza',
+    uri: 'pizza2'
+  },
+  {
+    category: 'burgers',
+    uri: 'burgers3'
+  },
+  {
+    category: 'chinese',
+    uri: 'chinese2'
+  },
+  {
+    category: 'steakhouse',
+    uri: 'steakhouse1'
+  },
+  {
+    category: 'cajun_creole',
+    uri: 'cajunCreole1'
+  }
+];
